4badc7c2159dc079e68f6131eee831119c80cc37
Ahmed Abdulghany <ahmedabdulghany7@gmail.com>
2025-11-27 22:55:52 +0200

    commit

---
+++ app_one/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': "Real Estate Products",
+    'version': "18.0.1.0.0",
+
+    'author': "Ahmed Abdulghany",
+    'category': "Inventory/Inventory",
+
+    'depends': ['product', 'stock'],
+    'data': [
+        'views/product_template_views.xml',
+    ],
+    'demo': [
+
+    ],
+
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': "LGPL-3",
+}
